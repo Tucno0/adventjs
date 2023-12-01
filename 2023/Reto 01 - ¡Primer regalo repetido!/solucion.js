20de440e49b704d53f497e3782ebff1bacbe89ab
Jhampier <yantucnoperez@gmail.com>
2023-12-01 15:15:18 -0500

    Reto 01 completado

---
+++ 2023/Reto 01 - ¡Primer regalo repetido!/solucion.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @param {Array} gifts
+ * @returns
+ */
+function findFirstRepeated(gifts) {
+  // Code here
+  let firstRepeatedId = -1;
+
+  for (const i in gifts) {
+    const arr = gifts.slice(0, i);
+
+    if (arr.includes(gifts[i])) {
+      firstRepeatedId = gifts[i];
+      break;
+    }
+  }
+
+  return firstRepeatedId;
+}
+
+const giftIds = [2, 1, 3, 5, 3, 2];
+const giftIds2 = [1, 2, 3, 4];
+const giftIds3 = [5, 1, 5, 1];
+
+console.log(findFirstRepeated(giftIds));
+console.log(findFirstRepeated(giftIds2));
+console.log(findFirstRepeated(giftIds3));
